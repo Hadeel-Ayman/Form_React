5de7c78f4d5c3e74340798aace7a8edc38eebf24
Hadeel-Ayman <haddelayman4@gmail.com>
2022-12-26 17:07:54 +0300

    add images

---
+++ src/images/index.js
@@ -0,0 +1,10 @@
+import GoogleIcon from './flat-color-icons_google.svg'
+import Github from "./Vectorgithub.png";
+import Ln from "./Vectorin.png";
+import Dots from "./Group2.png";
+import Twitter from "./Vectortwitter.png";
+import LogInImg from "./superscene-34-joystick_trans 1.png";
+import logInPassword from "./VectorpasswordIcon.png";
+export{
+    GoogleIcon, Dots,Github,Ln,Twitter,LogInImg,logInPassword
+}
