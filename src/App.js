24748cb09c3af193d12011d2b2bedeb5a149fee9
Hadeel-Ayman <haddelayman4@gmail.com>
2022-12-26 18:02:35 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
+import React from 'react';
+class App extends React.Component {
+  state = {
+    datashow: "LogIn",
+    Gmail: false
+  }
+  render() {
+    return (
+      <>
+        {this.state.datashow === "LogIn" ? <LogIn app={this} Gmail={this.state.Gmail} /> : <SignUp app={this} />}
+      </>
+    );
+  }
 }
 
 export default App;
