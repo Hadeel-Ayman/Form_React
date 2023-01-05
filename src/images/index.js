0b4cba471e6ffab6a307229cf6d58667dda81574
Hadeel-Ayman <haddelayman4@gmail.com>
2023-01-05 10:09:18 +0300

    add some images  in page

--- src/images/index.js
+++ src/images/index.js
@@ -1,10 +1,54 @@
-import GoogleIcon from './flat-color-icons_google.svg'
+import GoogleIcon from "./flat-color-icons_google.svg";
 import Github from "./Vectorgithub.png";
 import Ln from "./Vectorin.png";
 import Dots from "./Group2.png";
+import plus from "./plus.png";
+import like from './Like.png';
+import puzzle from './Puzzle.png';
+import light from './light.png';
+import Rectangle from "./Rectangle.png";
+import Sun from "./Sun.png";
+import moon from './moon.png';
+import FRIENDS from './FRIENDS.png';
+import winframe from './winframe.png';
+import win from './win.png';
+import trophy from './trophy.png';
+import play1 from './play1.png';
+import play2 from './play2.png';
+import play3 from './play3.png';
+import play4 from './play4.png';
+import Setting from "./Setting.png";
+import img1 from './img1.jpg';
+import img2 from './img2.png';
+import img3 from './img3.png'
 import Twitter from "./Vectortwitter.png";
 import LogInImg from "./superscene-34-joystick_trans 1.png";
 import logInPassword from "./VectorpasswordIcon.png";
-export{
-    GoogleIcon, Dots,Github,Ln,Twitter,LogInImg,logInPassword
-}
+export {
+  GoogleIcon,
+  light,
+  Rectangle,
+  moon,
+  Sun,
+  puzzle,
+  like,
+  Setting,
+  plus,
+  Dots,
+  Github,
+  Ln,
+  Twitter,
+  LogInImg,
+  logInPassword,
+  img1,
+  img2,
+  img3,
+  play1,
+  play2,
+  play3,
+  play4,
+  trophy,
+  FRIENDS,
+  win,
+  winframe
+};
